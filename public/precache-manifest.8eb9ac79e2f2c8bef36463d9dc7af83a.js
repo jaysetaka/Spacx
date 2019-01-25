3f16d59d5917ab600d2e883c1acb928bf48d3934
Jabulani Setaka <jsetaka@meditech.co.za>
2019-01-26 01:34:46 +0200

    final commit

---
+++ public/precache-manifest.8eb9ac79e2f2c8bef36463d9dc7af83a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "93a53ba9685d88c51c85",
+    "url": "/static/js/main.93a53ba9.chunk.js"
+  },
+  {
+    "revision": "cb2b239f13fea40feae7",
+    "url": "/static/js/1.cb2b239f.chunk.js"
+  },
+  {
+    "revision": "93a53ba9685d88c51c85",
+    "url": "/static/css/main.5f43ce4d.chunk.css"
+  },
+  {
+    "revision": "ef3618eec8f1a02631e0d5dde3a2d134",
+    "url": "/index.html"
+  }
+];
